d9b033259b61fb94854d374a5e81e363c1673c15
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-08-23 12:27:55 +0300

    Funtion 1: toggle play

--- src/hooks/useMusicePlayer.js
+++ src/hooks/useMusicePlayer.js
@@ -1,3 +1,21 @@
-const useMusicePlayer = () => {};
+import { useContext } from "react";
+import MusicContext from "../constexts/MusicContext";
+
+const useMusicePlayer = () => {
+  const [state, setState] = useContext(MusicContext);
+// Function 1: togglePlay
+  const togglePlay = () => {
+    if (state.isPlaying) {
+      setState({ ...state, isPlaying: false });
+      state.audioPlaying.pause();
+    }else{
+        setState({ ...state, isPlaying: true });
+        state.audioPlaying.play();
+    }
+  };
+  return {
+    togglePlay,
+  };
+};
 
 export { useMusicePlayer };
